136e69d4ff012a246a67146a8f45e2f155a72343
Francesca de Gruttola <francescadegruttola@MacBook-Air-di-Francesca.fritz.box>
2025-03-11 16:26:57 +0100

    update

--- models/staging_airports.sql
+++ models/staging_airports.sql
@@ -4,4 +4,18 @@ WITH airports_regions_join AS (
     LEFT JOIN {{source('flights_data', 'regions')}}
     USING (country)
 )
-SELECT * FROM airports_regions_join
+SELECT * FROM airports_regions_joins
+WITH airports_reorder AS (
+    SELECT country,
+           region,
+           faa,
+           name, 
+           lat,
+           lon,
+           alt,
+           tz,
+           dst,
+           city
+    FROM {{ ref('staging_airports') }}
+)
+SELECT * FROM airports_reorder;
