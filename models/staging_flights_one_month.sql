2c50dbed33e85817b748cc255437678c9815d621
Francesca de Gruttola <francescadegruttola@MacBook-Air-di-Francesca.fritz.box>
2025-03-11 16:07:48 +0100

    update

--- models/staging_flights_one_month.sql
+++ models/staging_flights_one_month.sql
@@ -5,4 +5,4 @@ WITH flights_one_month AS (
     FROM {{source('flights_data', 'flights')}}
     WHERE DATE_PART('month', flight_date) = 1 
 )
-SELECT * FROM flights_one_month
+SELECT * FROM flights_one_monthgit 
